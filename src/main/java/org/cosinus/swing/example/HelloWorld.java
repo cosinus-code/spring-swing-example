8cfe4a105307cebc3ffa42001e8069e5c177918b
Constantin Ciobotaru <constantin.ciobotaru@signanthealth.com>
2020-06-14 16:40:30 +0300

    first version of hello.world

---
+++ src/main/java/org/cosinus/swing/example/HelloWorld.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Cosinus Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cosinus.swing.example;
+
+import org.cosinus.swing.app.ApplicationFrame;
+import org.cosinus.swing.boot.SpringSwingApplication;
+import org.cosinus.swing.boot.SpringSwingBootApplication;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SpringSwingBootApplication
+public class HelloWorld extends ApplicationFrame {
+
+    @Override
+    public void initComponents() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JLabel(translate("hello.world"), SwingConstants.CENTER));
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        SpringSwingApplication.run(HelloWorld.class, args);
+    }
+}
---
+++ src/main/resources/spring-swing-example.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  APPLICATION_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+done
+APPLICATION_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+echo $APPLICATION_DIR
+
+"$JAVA_HOME/bin/java" \
+-jar $APPLICATION_DIR/spring-swing-example.jar \
+-splash-progress \
+-splash-progress-color=56,123,44 \
+-splash-progress-y=245 \
+-splash-progress-x=5
